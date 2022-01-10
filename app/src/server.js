097b4637fb8d9655b742cf6fb1d2e8d969a6b8e7
inductor69 <adityakiitdelhi@gmail.com>
2022-01-11 01:15:53 +0530

    update

--- app/src/server.js
+++ app/src/server.js
@@ -236,27 +236,16 @@ app.get('*', function (req, res) {
  * Check the functionality of STUN/TURN servers:
  * https://webrtc.github.io/samples/src/content/peerconnection/trickle-ice/
  */
-const iceServers =[
-	{ urls: "stun:stun.l.google.com:19302" },
-	{ urls: "stun:stun.stunprotocol.org:3478" },
-	{ urls: "stun:stun.sipnet.net:3478" },
-	{ urls: "stun:stun.ideasip.com:3478" },
-	{ urls: "stun:stun.iptel.org:3478" },
+const iceServers =  [
     {
-        urls: process.env.VUE_APP_STUN_URL,
-      },
-	{ urls: "turn:numb.viagenie.ca", username: "imvasanthv@gmail.com", credential: "d0ntuseme" },
-	{
-		urls: [
-			"turn:173.194.72.127:19305?transport=udp",
-			"turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
-			"turn:173.194.72.127:443?transport=tcp",
-			"turn:[2404:6800:4008:C01::7F]:443?transport=tcp",
-		],
-		username: "CKjCuLwFEgahxNRjuTAYzc/s6OMT",
-		credential: "u1SQDR/SQsPQIxXNWQT7czc/G4c=",
-	},
-];
+      urls: process.env.VUE_APP_STUN_URL,
+    },
+    {
+      urls: process.env.VUE_APP_TURN_URL,
+      username: process.env.VUE_APP_TURN_USER,
+      credential: process.env.VUE_APP_TURN_PASSWORD,
+    },
+  ];
 
 
 /**
