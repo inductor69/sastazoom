7267c67e7d934240ca3ba5c0909b90d00b92a609
inductor69 <adityakiitdelhi@gmail.com>
2022-01-11 01:10:07 +0530

    update

--- app/src/server.js
+++ app/src/server.js
@@ -237,23 +237,27 @@ app.get('*', function (req, res) {
  * https://webrtc.github.io/samples/src/content/peerconnection/trickle-ice/
  */
 const iceServers =[
+	{ urls: "stun:stun.l.google.com:19302" },
+	{ urls: "stun:stun.stunprotocol.org:3478" },
+	{ urls: "stun:stun.sipnet.net:3478" },
+	{ urls: "stun:stun.ideasip.com:3478" },
+	{ urls: "stun:stun.iptel.org:3478" },
+    {
+        urls: process.env.VUE_APP_STUN_URL,
+      },
+	{ urls: "turn:numb.viagenie.ca", username: "imvasanthv@gmail.com", credential: "d0ntuseme" },
 	{
-  iceTransportPolicy: "all",
-  reconnectTimer: 3000,
-
-  // These settings are no secret, since they are readable from the client side anyway
-  iceServers: [
-    {
-      urls: process.env.VUE_APP_STUN_URL,
-    },
-    {
-        urls: turnUrls,
-        username: turnUsername,
-        credential: turnCredential,
-    },
-  ],
-}
+		urls: [
+			"turn:173.194.72.127:19305?transport=udp",
+			"turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
+			"turn:173.194.72.127:443?transport=tcp",
+			"turn:[2404:6800:4008:C01::7F]:443?transport=tcp",
+		],
+		username: "CKjCuLwFEgahxNRjuTAYzc/s6OMT",
+		credential: "u1SQDR/SQsPQIxXNWQT7czc/G4c=",
+	},
 ];
+
 
 /**
  * Expose server to external with https tunnel using ngrok
